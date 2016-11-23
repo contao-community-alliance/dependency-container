0149eb13b3d273b018ea11bbdde709bbbe62f275
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-11-23 03:56:09 +0100

    Move contao resources and fix file headers
    Preparation for bundlifying this repository

---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of contao-community-alliance/dependency-container.
+ *
+ * (c) 2013-2016 Contao Community Alliance <https://c-c-a.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contao-community-alliance/dependency-container
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Tristan Lins <tristan@lins.io>
+ * @copyright  2013-2016 Contao Community Alliance <https://c-c-a.org>
+ * @license    https://github.com/contao-community-alliance/dependency-container/blob/master/LICENSE LGPL-3.0
+ * @link       https://github.com/contao-community-alliance/dependency-container
+ * @filesource
+ */
+
+$GLOBALS['TL_LANG']['MOD']['dependency-container'] = 'Dependency injection container';
