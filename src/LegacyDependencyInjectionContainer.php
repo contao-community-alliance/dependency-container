2553a6e2901a5d2e7231493cd9ecae30bd3d87f7
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-11-23 10:09:16 +0100

    Add symfony bundle files
    s

---
+++ src/LegacyDependencyInjectionContainer.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * This file is part of contao-community-alliance/dependency-container.
+ *
+ * (c) 2013-2016 Contao Community Alliance <https://c-c-a.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contao-community-alliance/dependency-container
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Tristan Lins <tristan@lins.io>
+ * @copyright  2013-2016 Contao Community Alliance <https://c-c-a.org>
+ * @license    https://github.com/contao-community-alliance/dependency-container/blob/master/LICENSE LGPL-3.0
+ * @link       https://github.com/contao-community-alliance/dependency-container
+ * @filesource
+ */
+
+namespace DependencyInjection\Container;
+
+use Contao\CoreBundle\Framework\ContaoFrameworkInterface;
+
+/**
+ * This class provides a gateway from the legacy dependency container to the symfony dependency container.
+ */
+class LegacyDependencyInjectionContainer
+{
+    /**
+     * The Contao framework.
+     *
+     * @var ContaoFrameworkInterface
+     */
+    private $framework;
+
+    /**
+     * Create a new instance.
+     *
+     * @param ContaoFrameworkInterface $framework The Contao framework.
+     */
+    public function __construct(ContaoFrameworkInterface $framework)
+    {
+        $this->framework = $framework;
+    }
+
+    /**
+     * Retrieve a service from pimple.
+     *
+     * @param string $serviceName The name of the service to retrieve.
+     *
+     * @return mixed
+     *
+     * @SuppressWarnings(PHPMD.Superglobals)
+     * @SuppressWarnings(PHPMD.CamelCaseVariableName)
+     */
+    public function getService($serviceName)
+    {
+        $this->framework->initialize();
+
+        return $GLOBALS['container'][$serviceName];
+    }
+}
---
+++ tests/Mocks/Bundles/TestBundle/TestBundle.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of contao-community-alliance/dependency-container.
+ *
+ * (c) 2013 Contao Community Alliance
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contao-community-alliance/dependency-container
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @copyright  2013-2015 Contao Community Alliance
+ * @license    https://github.com/contao-community-alliance/dependency-container/blob/master/LICENSE LGPL-3.0+
+ * @link       http://c-c-a.org
+ * @filesource
+ */
+
+namespace DependencyInjection\Container\Test\Mocks\Bundles\TestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * This is the bundle for the unit test.
+ */
+class TestBundle extends Bundle
+{
+}
