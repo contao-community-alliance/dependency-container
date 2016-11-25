2c88f325181ed5e510f4b1511bc3f810303493a9
David Molineus <david.molineus@netzmacht.de>
2016-11-25 16:19:00 +0100

    Initial draft of symfony container integration.

---
+++ src/DependencyInjection/ContaoCommunityAllianceDependencyContainerExtension.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This file is part of contao-community-alliance/dependency-container.
+ *
+ * (c) 2013-2016 Contao Community Alliance
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contao-community-alliance/dependency-container
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2013-2016 Contao Community Alliance
+ * @license    https://github.com/contao-community-alliance/dependency-container/blob/master/LICENSE LGPL-3.0+
+ * @link       http://c-c-a.org
+ * @filesource
+ */
+
+namespace DependencyInjection\Container\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * Class ContaoCommunityAllianceDependencyContainerExtension
+ *
+ * @package DependencyInjection\Container\DependencyInjection
+ */
+class ContaoCommunityAllianceDependencyContainerExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
