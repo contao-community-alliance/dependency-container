d1be849c0fb44aa00d2d58224bfebc9da27e71c7
Sven Baumann <baumann.sv@gmail.com>
2017-07-02 13:11:19 +0200

    Add phpunit test for contao manager plugin

---
+++ tests/ContaoManager/PluginTest.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * This file is part of contao-community-alliance/dependency-container.
+ *
+ * (c) 2013-2017 Contao Community Alliance <https://c-c-a.org>
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contao-community-alliance/dependency-container
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2013-2017 Contao Community Alliance <https://c-c-a.org>
+ * @license    https://github.com/contao-community-alliance/event-dispatcher/LICENSE LGPL-3.0+
+ * @link       https://github.com/contao-community-alliance/event-dispatcher
+ * @filesource
+ */
+
+
+namespace DependencyInjection\Container\Test\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerBundle\ContaoManagerBundle;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use DependencyInjection\Container\CcaDependencyInjectionBundle;
+use DependencyInjection\Container\ContaoManager\Plugin;
+
+/**
+ * Tests the Plugin class.
+ */
+class PluginTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Tests the object instantiation.
+     */
+    public function testInstantiation()
+    {
+        $plugin = new Plugin();
+
+        $this->assertInstanceOf('DependencyInjection\Container\ContaoManager\Plugin', $plugin);
+    }
+
+    /**
+     * Tests the getBundles() method.
+     */
+    public function testGetBundles()
+    {
+        $parser = $this->getMock(ParserInterface::class);
+
+        /** @var BundleConfig $config */
+        $config = (new Plugin())->getBundles($parser)[0];
+
+        $this->assertInstanceOf('Contao\ManagerPlugin\Bundle\Config\BundleConfig', $config);
+        $this->assertSame(CcaDependencyInjectionBundle::class, $config->getName());
+        $this->assertSame(
+            [
+                ContaoCoreBundle::class,
+                ContaoManagerBundle::class
+            ],
+            $config->getLoadAfter()
+        );
+    }
+}
