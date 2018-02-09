5bacd2e973d6691ddfadcbdfed43aef66562d812
Sven Baumann <baumann.sv@gmail.com>
2018-02-09 23:23:29 +0100

    Update the year in the php doc

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/dependency-container.
  *
- * (c) 2013-2016 Contao Community Alliance
+ * (c) 2013-2018 Contao Community Alliance
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -13,7 +13,8 @@
  * @package    contao-community-alliance/dependency-container
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Tristan Lins <tristan@lins.io>
- * @copyright  2013-2016 Contao Community Alliance
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2013-2018 Contao Community Alliance
  * @license    https://github.com/contao-community-alliance/dependency-container/blob/master/LICENSE LGPL-3.0+
  * @link       http://c-c-a.org
  * @filesource
