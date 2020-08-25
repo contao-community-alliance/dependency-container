730faa0cbfed288925985a78c856c65495c8a5bb
Sven Baumann <baumann.sv@gmail.com>
2020-08-25 19:26:59 +0200

    Fix the php doc

--- tests/DependencyInjection/CcaDependencyInjectionExtensionTest.php
+++ tests/DependencyInjection/CcaDependencyInjectionExtensionTest.php
@@ -3,16 +3,17 @@
 /**
  * This file is part of contao-community-alliance/dependency-container.
  *
- * (c) 2013-2018 Contao Community Alliance <https://c-c-a.org>
+ * (c) 2013-2020 Contao Community Alliance <https://c-c-a.org>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
- * @package    contao-community-alliance/dependency-container* @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @package    contao-community-alliance/dependency-container
  * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2013-2020Contao Community Alliance <https://c-c-a.org>
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2013-2020 Contao Community Alliance <https://c-c-a.org>
  * @license    https://github.com/contao-community-alliance/dependency-container/blob/master/LICENSE LGPL-3.0
  * @link       https://github.com/contao-community-alliance/dependency-container
  * @filesource
